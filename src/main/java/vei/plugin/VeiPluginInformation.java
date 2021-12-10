a1943cc0a2c606a9d25a3fd24c71005e0b9a8d5a
colin robertson <30844830+Wobblyyyy@users.noreply.github.com>
2021-12-10 17:10:39 -0500

    Create VeiPluginInformation.java

---
+++ src/main/java/vei/plugin/VeiPluginInformation.java
@@ -0,0 +1,19 @@
+package vei.plugin;
+
+public abstract class VeiPluginInformation {
+    public abstract String getTitle();
+    public abstract String getAuthor();
+    public abstract String getDescription();
+    public abstract int getMajorVersion();
+    public abstract int getMinorVersion();
+    public abstract int getPatchVersion();
+    
+    public String getVersion() {
+        return String.format(
+            "v%s.%s.%s",
+            getMajorVersion(),
+            getMinorVersion(),
+            getPatchVersion()
+        );
+    }
+}
