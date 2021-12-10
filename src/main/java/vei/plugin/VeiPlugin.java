4bcb118d77af105f4a08d5d37e132b27ace5a7ef
colin robertson <30844830+Wobblyyyy@users.noreply.github.com>
2021-12-10 17:18:00 -0500

    Update VeiPlugin.java

--- src/main/java/vei/plugin/VeiPlugin.java
+++ src/main/java/vei/plugin/VeiPlugin.java
@@ -2,13 +2,34 @@ package vei.plugin;
 
 import vei.commands.CommandRegistry;
 
-public interface VeiPlugin {
-    VeiPluginInformation getPluginInformation();
-    void onKeyPress(VeiPluginKeyPress keyPress);
-    void onFileOpen(VeiFileOpen fileOpen);
-    void onFileClose(VeiFileClose fileClose);
-    void onUnload(VeiPluginConfiguration configuration);
-    void registerNormalCommands(CommandRegistry registry);
-    void registerInsertCommands(CommandRegistry registry);
-    void registerVisualCommands(CommandRegistry registry);
+public abstract class VeiPlugin {
+    public abstract VeiPluginInformation getPluginInformation();
+    
+    public void onKeyPress(VeiPluginKeyPress keyPress) {
+        
+    }
+    
+    public void onFileOpen(VeiFileOpen fileOpen) {
+        
+    }
+    
+    public void onFileClose(VeiFileClose fileClose) {
+        
+    }
+    
+    public void onUnload(VeiPluginConfiguration configuration) {
+        
+    }
+    
+    public Iterable<Command> getNormalCommands() {
+        return null;
+    }
+    
+    public Iterable<Command> getInsertCommands() {
+        return null;
+    }
+    
+    public Iterable<Command> getVisualCommands() {
+        return null;
+    }
 }
